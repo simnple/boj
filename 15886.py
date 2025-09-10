5f7ff7248be654f1c8707ec37e7b46f5a18191dc
Simnple <75520229+simnple@users.noreply.github.com>
2025-09-10 22:14:56 +0900

    앞 W와 뒤 E는 조건상 확인을 안해도 됨

--- 15886.py
+++ 15886.py
@@ -3,12 +3,6 @@ MAP = list(input())
 
 count = 0
 
-if MAP[-1] == "E":
-    count += 1
-
-if MAP[0] == "W":
-    count += 1
-
 for i in range(N):
     if MAP[i:i+2] == ["E", "W"]:
         count += 1
