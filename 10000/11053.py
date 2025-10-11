a786aaa6321d97a159164e7e074c280d6285fe94
Simnple <75520229+simnple@users.noreply.github.com>
2025-10-12 02:11:21 +0900

    solved 11053

--- 10000/11053.py
+++ 10000/11053.py
@@ -1,8 +1,16 @@
 import sys
 
-def input(): return sys.stdin.readline().rstrip()
+input = sys.stdin.readline
 
 N = int(input())
 A = list(map(int, input().split()))
+dp = [0] * N
 
-for i in range(len(A)):
+for i in range(N):
+    max_len = 0
+    for j in range(i):
+        if A[j] < A[i] and (max_len == 0 or dp[j] > max_len):
+            max_len = dp[j]
+    dp[i] = max_len + 1
+
+print(max(dp))
