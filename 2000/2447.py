0a606b69d92ec3d3ccc89a19793f89b1eb2fad8e
Simnple <75520229+simnple@users.noreply.github.com>
2025-10-13 23:53:08 +0900

    solved 2447

--- 2000/2447.py
+++ 2000/2447.py
@@ -1,3 +1,31 @@
-def print_star(n):
-    if n == 1: 
-    # print
+def calculate_empty(n):
+    return [[" " * n] for _ in range(n)]
+
+def calculate_star(n):
+    if n == 3:
+        return [
+            ["***"],
+            ["* *"],
+            ["***"]
+        ]
+
+    else:
+        box = [
+            [calculate_star(n // 3), calculate_star(n // 3), calculate_star(n // 3)],
+            [calculate_star(n // 3), calculate_empty(n // 3), calculate_star(n // 3)],
+            [calculate_star(n // 3), calculate_star(n // 3), calculate_star(n // 3)]
+        ]
+        result = [[] for _ in range(n)]
+
+        for y in range(3):
+            for e in range(n // 3):
+                line = []
+                for x in range(3):
+                    line.append(box[y][x][e][0])
+                result[y * (n // 3) + e].append("".join(line))
+
+        return result
+
+n = int(input())
+for s in calculate_star(n):
+    print(s[0])
