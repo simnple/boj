90dc158aad1fbf273ee499dbf1f05fdd0223509d
Simnple <75520229+simnple@users.noreply.github.com>
2025-10-26 01:42:15 +0900

    edited 2670 for faster

--- 2000/2670.py
+++ 2000/2670.py
@@ -5,15 +5,13 @@ input = sys.stdin.readline
 N = int(input())
 A = [float(input()) for _ in range(N)]
 
-max_value = 0
-for i in range(N):
-    if A[i] == 0: continue
-    value = 1
+max_value = A[0]
+prev_value = A[0]
+for i in range(1, N):
+    value = max(A[i], prev_value * A[i])
+    prev_value = value
 
-    for j in range(i, N):
-        if A[j] == 0: break
-        value *= A[j]
-        if max_value < value:
-            max_value = value
+    if max_value < value:
+        max_value = value
 
 print(f"{max_value:.3f}")
