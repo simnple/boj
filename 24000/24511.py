6c8d26561984b6f8984ed582bc4cf7f2a3e7ecd1
Simnple <75520229+simnple@users.noreply.github.com>
2025-10-26 16:20:16 +0900

    solved 24511

--- 24000/24511.py
+++ 24000/24511.py
@@ -1,5 +1,19 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
 N = int(input())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
+queue = deque([B[i] for i in range(N) if A[i] == 0])
+
 M = int(input())
 C = list(map(int, input().split()))
+
+results = []
+for i in range(M):
+    queue.appendleft(C[i])
+    results.append(queue.pop())
+
+print(*results)
