7aa78a1a60778076caded314baae27562c48b9c0
Simnple <75520229+simnple@users.noreply.github.com>
2025-11-02 23:38:00 +0900

    solved 9251

---
+++ 9000/9251.py
@@ -0,0 +1,13 @@
+first = input()
+second = input()
+
+dp = [[0] * (len(first) + 1) for _ in range(len(second) + 1)]
+
+for i in range(1, len(second) + 1):
+    for j in range(1, len(first) + 1):
+        if first[j - 1] == second[i - 1]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+print(dp[len(second)][len(first)])
