30e9bbc5e7d776703e14d1a8ef518417147785db
Simnple <75520229+simnple@users.noreply.github.com>
2025-11-03 19:15:27 +0900

    solved 2566

--- 2000/2566.py
+++ 2000/2566.py
@@ -1,11 +1,11 @@
 nums = [list(map(int, input().split())) for _ in range(9)]
 
-result = [-1, 0, 0]
+pos = (0, 0)
 
 for y in range(9):
     for x in range(9):
-        if nums[y][x] > result[0]:
-            result = [nums[y][x], y+1, x+1]
+        if nums[y][x] > nums[pos[0]][pos[1]]:
+            pos = [y, x]
 
-print(result[0])
-print(*result[1:])
+print(nums[pos[0]][pos[1]])
+print(*[i + 1 for i in pos])
