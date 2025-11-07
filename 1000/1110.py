ea12e88f1a6a58b1ff466403e795960c5ae9f663
Simnple <75520229+simnple@users.noreply.github.com>
2025-11-07 14:35:04 +0900

    solved 1110

--- 1000/1110.py
+++ 1000/1110.py
@@ -1,19 +1,14 @@
-_ = input()
+def plus_cycle(n):
+    s1, s2 = n // 10, n % 10
+    return int(f"{s2}{(s1 + s2) % 10}")
 
-if len(_) == 1: A, B = int(_), 0
-else: A, B = map(int, list(_))
+N = int(input())
 
-original_num = A * 10 + B
-temp_num = -1
-count = 0
+num = plus_cycle(N)
+count = 1
 
-while original_num != temp_num:
+while N != num:
+    num = plus_cycle(num)
     count += 1
-
-    temp = int(str(A + B)[-1])
-    A = B
-    B = temp
-
-    temp_num = A * 10 + B
 
 print(count)
