0a20e9b4bd5fe51151f510d53f1c6f3ef1d5b7ed
Simnple <75520229+simnple@users.noreply.github.com>
2025-11-09 22:35:03 +0900

    solved 10844

--- 10000/10844.py
+++ 10000/10844.py
@@ -1,3 +1,22 @@
+MOD = 1_000_000_000
+
 N = int(input())
 
-for i in range(N):
+# 0 1 1 1 1 1 1 1 1 1
+dp = [1] * 10
+dp[0] = 0
+
+for i in range(1, N):
+    temp = [0] * 10
+
+    for j in range(10):
+        if j == 0:
+            temp[1] += dp[j] % MOD
+        elif j == 9:
+            temp[8] += dp[j] % MOD
+        else:
+            temp[j - 1] += dp[j] % MOD
+            temp[j + 1] += dp[j] % MOD
+    dp = temp.copy()
+
+print(sum(dp) % MOD)
