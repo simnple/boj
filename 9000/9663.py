2c488f5422f513f72c06676d88b3f3959d431b8b
Simnple <75520229+simnple@users.noreply.github.com>
2025-11-12 21:26:10 +0900

    solved 9663

--- 9000/9663.py
+++ 9000/9663.py
@@ -1,36 +1,29 @@
-count = 0
+def n_queen(queens):
+    size = len(queens)
+    if size == N:
+        return 1
+    
+    x_list = [True] * N
 
-def n_queen(x_pos, y_pos):
-    global count
-    global N
+    for y in range(size):
+        x = queens[y]
 
-    size = len(x_pos)
-    if N == size:
-        # print("1")
-        count += 1
+        x_list[x] = False
+        diff = abs(y - size)
 
-    elif size == 0:
-        for y in range(N):
-            for x in range(N):
-                n_queen(x_pos + [x], y_pos + [y])
+        if 0 <= x + diff < N:
+            x_list[x + diff] = False
+        if 0 <= x - diff < N:
+            x_list[x - diff] = False
 
-    else:
-        for y in range(N):
-            if y in y_pos: continue
-            for x in range(N):
-                if x in x_pos: continue
+    result = 0
+    for x in range(N):
+        if not x_list[x]: continue
 
-                need_escape = False
-                for i in range(size):
-                    for j in range(-N, N+1):
-                        if x_pos[i] + j == x and y_pos[i] + j == y:
-                            need_escape = True
-                            break
-                    if need_escape: break
-                if need_escape: continue
+        result += n_queen(queens + [x])
 
-                n_queen(x_pos + [x], y_pos + [y])
+    return result
 
 N = int(input())
-n_queen([], [])
-print(count)
+
+print(n_queen([]))
