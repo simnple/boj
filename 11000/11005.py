3f04388ff79516f4caf143d4043c8bcf69890ee9
Simnple <75520229+simnple@users.noreply.github.com>
2025-11-19 22:28:12 +0900

    solved 11005

--- 11000/11005.py
+++ 11000/11005.py
@@ -1,5 +1,5 @@
-def solution(n, base):
-    chars = [chr(i) for i in range(48, 58)] + [chr(i) for i in range(65, 91)]
+def base_converter(n, base):
+    chars = [str(i) for i in range(10)] + [chr(i) for i in range(65, 91)]
 
     result = []
     while n > 0:
@@ -8,4 +8,4 @@ def solution(n, base):
     return "".join(result[::-1])
 
 N, B = map(int, input().split())
-print(solution(N, B))
+print(base_converter(N, B))
