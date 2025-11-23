be543716c3d6e31a23ae02c9302d59f01644a705
Simnple <75520229+simnple@users.noreply.github.com>
2025-11-23 17:29:40 +0900

    solved 34115

--- 34000/34115.py
+++ 34000/34115.py
@@ -1,11 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
 N = int(input())
 X = list(map(int, input().split()))
 
-nums = [0] * (N + 1)
+start_idx = [-1] * (N + 1)
+end_idx = [-1] * (N + 1)
 
-for i in range(len(X)):
-    # i + 1
+for i in range(2 * N):
+    if start_idx[X[i]] == -1:
+        start_idx[X[i]] = i
+    else:
+        end_idx[X[i]] = i
 
-    idx = X[i]
-    if nums[idx] == 0:
-        
+K = max(map(lambda x: x[1] - x[0] - 1, zip(start_idx, end_idx)))
+
+print(K)
