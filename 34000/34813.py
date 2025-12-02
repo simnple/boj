925699cc3858f47563658861b33dd33d584645df
simnple <75520229+simnple@users.noreply.github.com>
2025-12-02 14:41:29 +0900

    solved 34813

---
+++ 34000/34813.py
@@ -0,0 +1,10 @@
+S = input()
+
+if S[0] == "F":
+    print("Foundation")
+elif S[0] == "C":
+    print("Claves")
+elif S[0] == "V":
+    print("Veritas")
+elif S[0] == "E":
+    print("Exploration")
