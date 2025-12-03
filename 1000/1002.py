fb74a02bc20d9ad2738c1f13b5ae964ab60116fb
Simnple <75520229+simnple@users.noreply.github.com>
2025-12-04 00:50:26 +0900

    solved 1002

--- 1000/1002.py
+++ 1000/1002.py
@@ -1,10 +1,15 @@
 T = int(input())
 
 for _ in range(T):
-    x1, y1, r1, x2, y2, r2 = map(int, input().split())
-    if x1 == x2 and y1 == y2: # 시작이 같을 때
-        if r1 == r2: print(-1)
-        else: print(0)
-    else: # 그 외는 1개 만나거나 2개 만남
-        # 1개일땐 서로 만나야함 -> 두 원의 중점을 지나는 직선의 방정식 구하고 그 직선의 방정식과 한 원이 지나는 그 점이 다른 원이 지나는 점이랑 같아야함
-        
+    x_1, y_1, r_1, x_2, y_2, r_2 = map(int, input().split())
+
+    distance = (x_2 - x_1) ** 2 + (y_2 - y_1) ** 2
+
+    if x_1 == x_2 and y_1 == y_2 and r_1 == r_2:
+        print(-1)
+    elif abs(r_1 - r_2) ** 2 < distance < (r_1 + r_2) ** 2:
+        print(2)
+    elif distance == abs(r_1 - r_2) ** 2 or distance == (r_1 + r_2) ** 2:
+        print(1)
+    else:
+        print(0)
