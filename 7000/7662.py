7763c4179a074f72e6a56ed56d0cf135a5d5a738
Simnple <75520229+simnple@users.noreply.github.com>
2025-12-19 17:21:15 +0900

    solved 7662

--- 7000/7662.py
+++ 7000/7662.py
@@ -6,26 +6,44 @@ def input(): return sys.stdin.readline().rstrip()
 T = int(input())
 
 for _ in range(T):
+    max_heap = []
+    min_heap = []
+
     k = int(input())
-    heap = []
+    visited = [0] * k
 
-    for __ in range(k):
-        cmd, num = input().split()
-        num = int(num)
+    for i in range(k):
+        operator, value = input().split()
+        value = int(value)
 
-        if cmd == "I":
-            heapq.heappush(heap, num)
+        if operator == "I":
+            heapq.heappush(max_heap, (-value, i))
+            heapq.heappush(min_heap, (value, i))
+            visited[i] = 1
 
-        elif cmd =="D" and heap:
-            if num == 1:
-                heap.pop()
+        else:
+            if value == 1:
+                while max_heap and visited[max_heap[0][1]] == 0:
+                    heapq.heappop(max_heap)
+                if max_heap:
+                    pop_value, idx = heapq.heappop(max_heap)
+                    visited[idx] = 0
 
             else:
-                heapq.heappop(heap)
+                while min_heap and visited[min_heap[0][1]] == 0:
+                    heapq.heappop(min_heap)
+                if min_heap:
+                    pop_value, idx = heapq.heappop(min_heap)
+                    visited[idx] = 0
 
-    if len(heap):
-        print(heap)
-        print(heap[-1], heap[0])
+    while max_heap and visited[max_heap[0][1]] == 0:
+        heapq.heappop(max_heap)
 
+    while min_heap and visited[min_heap[0][1]] == 0:
+        heapq.heappop(min_heap)
+    
+    if max_heap and min_heap:
+        print(-max_heap[0][0], min_heap[0][0])
+    
     else:
         print("EMPTY")
